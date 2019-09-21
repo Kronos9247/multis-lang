28b1eda5da04523a7cdd4296b1f00ba793bbb6d4
Rafael Orman <rafael@orman.at>
2019-09-21 22:17:12 +0200

    change default container

--- js/multis/ops/if.js
+++ js/multis/ops/if.js
@@ -1,62 +1,17 @@
 if (typeof multis === "undefined") multis = {};
 
-class IfStore extends multis.Container {
-    constructor(parent) {
-        super(parent);
-
-        this.state = null;
-        this.suppress = false;
-    }
-
-    suppress() {
-        this.suppress = true;
-    }
-
-    store(state) {
-        if (!this.suppress) {
-            if (this.state !== null) {
-                state.destroy();
-
-                editor.stdout.throw(new Error("multiple states on the same operand"));
-            }
-            else 
-                this.state = state;
-        }
-        else {
-            if (this.state === null)
-                this.state = [];
-
-            let findstate = this.state.find((v) => v == state);
-            if (findstate === null)
-                this.state.push(state);
-        }
-    }
-
-    each(cb) {
-        // I dont like to handle multiple states, so simply create a each method for that
-        if (this.state === null)
-            return ;
-
-        if (Array.isArray(this.state)) {
-            for (let i = 0; i < this.state.length; i++) {
-                cb(this.state[i]);
-            }
-        }
-        else {
-            cb(this.state);
-        }
-    }
-
-    empty() {
-        this.state = null;
-    }
-}
+// class IfStore extends multis.Container {
+//     constructor(parent) {
+//         super(parent);
+//     }
+// }
 
 class IfOp extends multis.Event {
     constructor() {
         super();
 
-        this.use((e) => new IfStore(e));
+        // this.use((e) => new IfStore(e));
+        this.use((e) => new multis.Container(e));
     }
 
     onStep(state) {
